a20ce95f4d51236dea9e1eedfc2ba27edb91ab20
ebotiab <ebotia@pa.uc3m.es>
2022-03-17 18:09:44 +0100

    Refactor extract_from_zip.py to extract_from_compressed.py and update to include tar files in extraction

--- src/data/extract_from_compressed.py
+++ src/data/extract_from_compressed.py
@@ -1,7 +1,7 @@
 import fnmatch
 import os
 import zipfile
-
+import tarfile
 
 """
 A script iterates through a directory of the 189 DAIC-WOZ participant zip
@@ -9,7 +9,7 @@ files and extracts the wav and transcript files.
 """
 
 
-def extract_files(zip_file, out_dir, delete_zip=False):
+def extract_files(compressed_file, out_dir, is_tar=True, delete_zip=False):
     """
     A function takes in a zip file and extracts the .wav file and
     *TRANSCRIPT.csv files into separate folders in a user
@@ -17,11 +17,13 @@ def extract_files(zip_file, out_dir, delete_zip=False):
 
     Parameters
     ----------
-    zip_file : filepath
+    compressed_file : filepath
         path to the folder containing the DAIC-WOZ zip files
     out_dir : filepath
         path to the desired directory where audio and transcript folders
         will be created
+    is_tar : bool
+        If true, interprets compressed file as a tar, interprets compressed file as zip otherwise
     delete_zip : bool
         If true, deletes the zip file once relevant files are extracted
 
@@ -41,30 +43,39 @@ def extract_files(zip_file, out_dir, delete_zip=False):
     if not os.path.exists(audio_dir):
         os.makedirs(transcripts_dir)
 
-    zip_ref = zipfile.ZipFile(zip_file)
-    for f in zip_ref.namelist():  # iterate through files in zip file
+    if is_tar:  # create object from zip and extract member names
+        compressed_ref = tarfile.open(compressed_file, 'r')
+        file_names = compressed_ref.getnames()
+    else:
+        compressed_ref = zipfile.ZipFile(compressed_file)
+        file_names = compressed_ref.namelist()
+
+    for f in file_names:  # iterate through files in compressed file
         if f.endswith('.wav'):
-            zip_ref.extract(f, audio_dir)
-        elif fnmatch.fnmatch(f, '*TRANSCRIPT.csv'):
-            zip_ref.extract(f, transcripts_dir)
-    zip_ref.close()
+            compressed_ref.extract(f, audio_dir)
+        elif fnmatch.fnmatch(f.lower(), '*transcript.csv'):
+            compressed_ref.extract(f, transcripts_dir)
+    compressed_ref.close()
 
     if delete_zip:
-        os.remove(zip_file)
+        os.remove(compressed_file)
 
 
 if __name__ == '__main__':
-    # directory containing DIAC-WOZ zip files
-    dir_name = '/Volumes/Seagate Backup Plus Drive/DAIC-WOZ/'
+    # directory containing DAIC-WOZ zip files
+    dir_name = '/home/ebbarbera/experiment/'
 
     # directory where audio and transcripts folders will be created
-    out_dir = '../../depression-detect/data/raw'
+    out_path = '/../../depression-detect/data/raw'
 
     # delete zip file after file wav and csv extraction
-    delete_zip = False
+    remove_zip = False
 
     # iterate through zip files in dir_name and extracts wav and transcripts
     for file in os.listdir(dir_name):
-        if file.endswith('.zip'):
+        if file.endswith('.tar'):
             zip_file = os.path.join(dir_name, file)
-            extract_files(zip_file, out_dir, delete_zip=delete_zip)
+            extract_files(zip_file, out_path, is_tar=True, delete_zip=remove_zip)
+        elif file.endswith('.zip'):
+            zip_file = os.path.join(dir_name, file)
+            extract_files(zip_file, out_path, is_tar=False, delete_zip=remove_zip)
