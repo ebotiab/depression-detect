55b29ed4238a788aa59f28cfaaa0e2c144be742a
ebotiab <ebotia@pa.uc3m.es>
2022-03-25 10:11:16 +0100

    Fix bugs and warnings in spectrograms.py script

--- src/features/spectrograms.py
+++ src/features/spectrograms.py
@@ -11,47 +11,48 @@ to the CNN. This was heavily adopted from Frank Zalkow's work.
 """
 
 
-def stft(sig, frameSize, overlapFac=0.5, window=np.hanning):
+def stft(sig, frame_size, overlap_fac=0.5, window=np.hanning):
     """
     Short-time Fourier transform of audio signal.
     """
-    win = window(frameSize)
-    hopSize = int(frameSize - np.floor(overlapFac * frameSize))
+    win = window(frame_size)
+    hop_size = int(frame_size - np.floor(overlap_fac * frame_size))
     # zeros at beginning (thus center of 1st window should be for sample nr. 0)
-    samples = np.append(np.zeros(np.floor(frameSize/2.0)), sig)
+    samples = np.append(np.zeros(int(np.floor(frame_size / 2.0))), sig)
     # cols for windowing
-    cols = np.ceil((len(samples) - frameSize) / float(hopSize)) + 1
+    cols = int(np.ceil((len(samples) - frame_size) / float(hop_size)) + 1)
     # zeros at end (thus samples can be fully covered by frames)
-    samples = np.append(samples, np.zeros(frameSize))
+    samples = np.append(samples, np.zeros(frame_size))
 
-    frames = stride_tricks.as_strided(samples, shape=(cols, frameSize),
-                                      strides=(samples.strides[0]*hopSize,
-                                      samples.strides[0])).copy()
+    frames = stride_tricks.as_strided(samples, shape=(cols, frame_size),
+                                      strides=(samples.strides[0] * hop_size,
+                                               samples.strides[0])).copy()
     frames *= win
 
     return np.fft.rfft(frames)
 
 
-def logscale_spec(spec, sr=44100, factor=20.):
+def log_scale_spec(spec, sr=44100, factor=20.):
     """
     Scale frequency axis logarithmically.
     """
-    timebins, freqbins = np.shape(spec)
+    time_bins, freq_bins = np.shape(spec)
 
-    scale = np.linspace(0, 1, freqbins) ** factor
-    scale *= (freqbins-1)/max(scale)
+    scale = np.linspace(0, 1, freq_bins) ** factor
+    scale *= (freq_bins - 1) / max(scale)
     scale = np.unique(np.round(scale))
+    scale = scale.astype(int)
 
     # create spectrogram with new freq bins
-    newspec = np.complex128(np.zeros([timebins, len(scale)]))
+    new_spec = np.complex128(np.zeros([time_bins, len(scale)]))
     for i in range(0, len(scale)):
         if i == len(scale)-1:
-            newspec[:, i] = np.sum(spec[:, scale[i]:], axis=1)
+            new_spec[:, i] = np.sum(spec[:, scale[i]:], axis=1)
         else:
-            newspec[:, i] = np.sum(spec[:, scale[i]:scale[i+1]], axis=1)
+            new_spec[:, i] = np.sum(spec[:, scale[i]:scale[i + 1]], axis=1)
 
     # list center freq of bins
-    allfreqs = np.abs(np.fft.fftfreq(freqbins*2, 1./sr)[:freqbins+1])
+    allfreqs = np.abs(np.fft.fftfreq(freq_bins * 2, 1. / sr)[:freq_bins + 1])
     freqs = []
     for i in range(0, len(scale)):
         if i == len(scale)-1:
@@ -59,10 +60,10 @@ def logscale_spec(spec, sr=44100, factor=20.):
         else:
             freqs += [np.mean(allfreqs[scale[i]:scale[i+1]])]
 
-    return newspec, freqs
+    return new_spec, freqs
 
 
-def stft_matrix(audiopath, binsize=2**10, png_name='tmp.png',
+def stft_matrix(audio_path, bin_size=2**10, png_name='tmp.png',
                 save_png=False, offset=0):
     """
     A function that converts a wav file into a spectrogram represented by a \
@@ -70,12 +71,12 @@ def stft_matrix(audiopath, binsize=2**10, png_name='tmp.png',
     the values of the matrix represent the decibel intensity. A matrix of \
     this form can be passed as input to the CNN after undergoing normalization.
     """
-    samplerate, samples = wav.read(audiopath)
-    s = stft(samples, binsize)
+    samplerate, samples = wav.read(audio_path)
+    s = stft(samples, bin_size)
 
-    sshow, freq = logscale_spec(s, factor=1, sr=samplerate)
+    sshow, freq = log_scale_spec(s, sr=samplerate, factor=1)
     ims = 20.*np.log10(np.abs(sshow)/10e-6)  # amplitude to decibel
-    timebins, freqbins = np.shape(ims)
+    time_bins, freq_bins = np.shape(ims)
 
     ims = np.transpose(ims)
     ims = np.flipud(ims)  # weird - not sure why it needs flipping
@@ -99,7 +100,7 @@ if __name__ == '__main__':
     # directory containing participant folders with segmented wav files
     dir_name = '../../data/interim'
 
-    # walks through wav files in dir_name and creates pngs of the spectrograms.
+    # walks through wav files in dir_name and creates pngs of the spectrogram's.
     # This is a visual representation of what is passed to the CNN before
     # normalization, although a cropped matrix representation is actually
     # passed.
